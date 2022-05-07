dc9a5620ec3df7f6efc5a5f7658e2404c0b67529
sheben404 <sheben404@gmail.com>
2022-05-07 18:53:25 +0800

    feat: 处理 response 中的 data（transformResponseData）

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { transformRequest } from "./helpers/data";
+import { transformRequest, transformResponse } from "./helpers/data";
 import { processHeaders } from "./helpers/headers";
 import { buildURL } from "./helpers/url";
-import { AxiosPromise, AxiosRequestConfig } from "./types";
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from "./types";
 import xhr from "./xhr";
 
 export default function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
 }
 
 function processConfig(config: AxiosRequestConfig): void {
@@ -27,5 +29,10 @@ function transformRequestData(config: AxiosRequestConfig): any {
 function transformHeaders(config: AxiosRequestConfig): any {
   const { headers = {}, data } = config
   return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): any {
+  res.data = transformResponse(res.data)
+  return res
 }
 
