74b7a744c8f77e30126e2f0925a14b707bff6ef0
sheben404 <sheben404@gmail.com>
2022-05-07 19:24:29 +0800

    feat: 错误信息增强（AxiosError）

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,5 @@
-import { transformRequest, transformResponse } from "./helpers/data";
-import { processHeaders } from "./helpers/headers";
-import { buildURL } from "./helpers/url";
-import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from "./types";
-import xhr from "./xhr";
+import axios from "./axios";
 
-export default function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
-}
+export * from './types'
 
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformUrl(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-function transformUrl(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-function transformResponseData(res: AxiosResponse): any {
-  res.data = transformResponse(res.data)
-  return res
-}
-
+export default axios
