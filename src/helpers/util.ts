f624df1fb9fdfdd6c0dc3d90b00bf972d047fe4c
sheben404 <sheben404@gmail.com>
2022-05-07 16:05:03 +0800

    fix: 使用 isPlainObject 代替 isObject

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -4,6 +4,14 @@ export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
+// 对于 FormData、ArrayBuffer 这些类型，
+// isObject 判断也为 true
 export function isObject(val: any): val is Object {
   return val !== null && typeof val === 'object'
+}
+
+// 而 isPlainObject 的判断方式，
+// 只有我们定义的普通 JSON 对象才能满足。
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
 }
