e7dade52a457673cc131fb6acf498899e2cc3ddf
sheben404 <sheben404@gmail.com>
2022-05-07 16:09:12 +0800

    feat: 处理请求 body 中的 data

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { transformRequest } from "./helpers/data";
 import { buildURL } from "./helpers/url";
 import { AxiosRequestConfig } from "./types";
 import xhr from "./xhr";
@@ -9,10 +10,15 @@ export default function axios(config: AxiosRequestConfig) {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
 }
 
